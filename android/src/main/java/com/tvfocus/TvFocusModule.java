85a8a702ac54a3a72fffedc8caae631c39318c1c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-11-18 17:48:21 +0100

    :sparkles: updateView Android implementation

--- android/src/main/java/com/tvfocus/TvFocusModule.java
+++ android/src/main/java/com/tvfocus/TvFocusModule.java
@@ -1,19 +1,45 @@
 package com.tvfocus;
 
 import androidx.annotation.NonNull;
+import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.uimanager.UIImplementation;
+import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.uimanager.IllegalViewOperationException;
 
 @ReactModule(name = TvFocusModule.NAME)
 public class TvFocusModule extends ReactContextBaseJavaModule {
   public static final String NAME = "TvFocus";
 
+  private UIManagerModule uiManager = null;
+  private UIImplementation uiImplementation = null;
+  private final ReactApplicationContext context;
+
+  private UIManagerModule getUIManager() {
+    if (uiManager == null) {
+      uiManager = context.getNativeModule(UIManagerModule.class);
+    }
+
+    return uiManager;
+  }
+
+  private UIImplementation getUIImplementation() {
+    if (uiImplementation == null) {
+      uiImplementation = this.getUIManager().getUIImplementation();
+    }
+
+    return uiImplementation;
+  }
+
   public TvFocusModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    context = reactContext;
   }
 
   @Override
@@ -22,11 +48,16 @@ public class TvFocusModule extends ReactContextBaseJavaModule {
     return NAME;
   }
 
-
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+  public void updateView(int tag, String className, ReadableMap props) {
+    try {
+      this.getUIImplementation().updateView(
+        tag,
+        className,
+        props
+      );
+    } catch (IllegalViewOperationException ignore) {
+      Log.w(this.getName(), "Failed updating view for tag: " + tag);
+    }
   }
 }
