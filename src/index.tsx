6d70d2d0a24c05121a10883ade161435afcf3dd2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-11-18 19:10:58 +0100

    :art: Simplify implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,15 @@
 import { NativeModules, Platform } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react-native-tv-focus' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-const TvFocus =
-  Platform.OS === 'android'
-    ? NativeModules.TvFocus
-      ? NativeModules.TvFocus
-      : new Proxy(
-          {},
-          {
-            get() {
-              throw new Error(LINKING_ERROR);
-            },
-          }
-        )
-    : NativeModules.UIManager;
-
 const updateView = (
   viewTag: number,
   className: string,
   props: Record<string, any>
 ) => {
-  TvFocus.updateView(viewTag, className, props);
+  if (Platform.OS === 'android') {
+    NativeModules.TvFocus.updateView(viewTag, className, props);
+  } else {
+    NativeModules.UIManager.updateView(viewTag, className, props);
+  }
 };
 
 export const focus = (tag: number | null | undefined) => {
